a89811413f7e15ebcfa6383bdc919d74e9a32d05
AVK93 <avk93@gmx.de>
2017-04-17 22:20:42 +0200

    Recovering from interrupted update

--- daemon.sh
+++ daemon.sh
@@ -1,10 +1,23 @@
 #!/usr/bin/env bash
 
 trap 'kill -TERM $PID' TERM INT
-rm /opt/JDownloader/JDownloader.jar.*
-rm /opt/JDownloader/JDownloader.pid
+rm -f /opt/JDownloader/JDownloader.jar.*
+rm -f /opt/JDownloader/JDownloader.pid
+
+# Check if JDownloader.jar exists, or if there is an interrupted update
+if [ ! -f /opt/JDownloader/JDownloader.jar ] && [ -f /opt/JDownloader/tmp/update/self/JDU/JDownloader.jar ]; then
+    cp /opt/JDownloader/tmp/update/self/JDU/JDownloader.jar /opt/JDownloader/
+fi
+
 java -Djava.awt.headless=true -jar /opt/JDownloader/JDownloader.jar &
 PID=$!
 wait $PID
 wait $PID
+
+# Debugging helper - if the container crashes, create a file called "jdownloader-block.txt" in the download folder
+# The container will not terminate (and you can run "docker exec -it ... bash")
+if [ -f /root/Downloads/jdownloader-block.txt ]; then
+    sleep 1000000
+fi
+
 EXIT_STATUS=$?
