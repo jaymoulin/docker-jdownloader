f95ad5a05a08e865fcae7f6477257f64c489656a
Jay MOULIN <jaymoulin@gmail.com>
2018-11-08 23:41:57 +0100

    jdownloader-block debugger should be first task

--- daemon.sh
+++ daemon.sh
@@ -4,6 +4,12 @@ trap 'kill -TERM $PID' TERM INT
 rm -f /opt/JDownloader/JDownloader.jar.*
 rm -f /opt/JDownloader/JDownloader.pid
 
+# Debugging helper - if the container crashes, create a file called "jdownloader-block.txt" in the download folder
+# The container will not terminate (and you can run "docker exec -it ... bash")
+if [ -f /root/Downloads/jdownloader-block.txt ]; then
+    sleep 1000000
+fi
+
 # Check if JDownloader.jar exists, or if there is an interrupted update
 if [ ! -f /opt/JDownloader/JDownloader.jar ] && [ -f /opt/JDownloader/tmp/update/self/JDU/JDownloader.jar ]; then
     cp /opt/JDownloader/tmp/update/self/JDU/JDownloader.jar /opt/JDownloader/
@@ -13,11 +19,5 @@ java -Djava.awt.headless=true -jar /opt/JDownloader/JDownloader.jar &
 PID=$!
 wait $PID
 wait $PID
-
-# Debugging helper - if the container crashes, create a file called "jdownloader-block.txt" in the download folder
-# The container will not terminate (and you can run "docker exec -it ... bash")
-if [ -f /root/Downloads/jdownloader-block.txt ]; then
-    sleep 1000000
-fi
 
 EXIT_STATUS=$?
