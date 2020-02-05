27f626fe4f127b7ab7af270e603deff7ac6f860d
Jay MOULIN <jaymoulin@gmail.com>
2020-02-05 13:36:00 +0100

    Fixes configure with env vars

--- daemon.sh
+++ daemon.sh
@@ -5,7 +5,7 @@ rm -f /opt/JDownloader/JDownloader.jar.*
 rm -f /opt/JDownloader/JDownloader.pid
 
 if [ ! -z "$MYJD_USER" ] && [ ! -z "$MYJD_PASSWORD" ]; then
-    configure $MYJD_USER $MYJD_PASSWORD
+    configure "$MYJD_USER" "$MYJD_PASSWORD"
 fi
 
 # Debugging helper - if the container crashes, create a file called "jdownloader-block.txt" in the download folder
