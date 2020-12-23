ac56206feee432e4c171756c5f7261ee3d9d1795
Thomas Deutsch <thomas@tuxpeople.org>
2020-12-23 11:42:23 +0100

    change ci to github actions

--- .travis/run-docker-linter.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -eu
-
-echo "now linting: $1"
-docker run --rm -i hadolint/hadolint hadolint - < "$1"
-echo "-------"
-
-
-# --ignore DL3006
--- .travis/run-linters.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-docker pull hadolint/hadolint
-
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-
-find ./ -name Dockerfile -print0 | xargs -0 -L1 $DIR/run-docker-linter.sh
