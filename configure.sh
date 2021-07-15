bf6a60d913484f6ffaaa4c521ac90590ab9f03f1
Jay MOULIN <jaymoulin@gmail.com>
2021-07-16 00:49:55 +0200

    Use app folder for JDownloader files

--- configure.sh
+++ configure.sh
@@ -7,10 +7,10 @@ if [ ! $# -eq 2 ]; then
     exit 1
 fi
 
-if [ ! -f /opt/JDownloader/cfg/org.jdownloader.api.myjdownloader.MyJDownloaderSettings.json ]; then
-    cp /opt/JDownloader/org.jdownloader.api.myjdownloader.MyJDownloaderSettings.json.dist /opt/JDownloader/cfg/org.jdownloader.api.myjdownloader.MyJDownloaderSettings.json
+if [ ! -f /opt/JDownloader/app/cfg/org.jdownloader.api.myjdownloader.MyJDownloaderSettings.json ]; then
+    cp /opt/JDownloader/org.jdownloader.api.myjdownloader.MyJDownloaderSettings.json.dist /opt/JDownloader/app/cfg/org.jdownloader.api.myjdownloader.MyJDownloaderSettings.json
 fi
 
-sed -Ei "s/\"password\" : .+\"(,?)/\"password\" : \"$2\"\1/" /opt/JDownloader/cfg/org.jdownloader.api.myjdownloader.MyJDownloaderSettings.json && \
-sed -Ei "s/\"email\" : .+\"(,?)/\"email\" : \"$1\"\1/" /opt/JDownloader/cfg/org.jdownloader.api.myjdownloader.MyJDownloaderSettings.json
+sed -Ei "s/\"password\" : .+\"(,?)/\"password\" : \"$2\"\1/" /opt/JDownloader/app/cfg/org.jdownloader.api.myjdownloader.MyJDownloaderSettings.json && \
+sed -Ei "s/\"email\" : .+\"(,?)/\"email\" : \"$1\"\1/" /opt/JDownloader/app/cfg/org.jdownloader.api.myjdownloader.MyJDownloaderSettings.json
 pkill -f "JDownloader"
--- daemon.sh
+++ daemon.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 trap 'kill -TERM $PID' TERM INT
-rm -f /opt/JDownloader/JDownloader.jar.*
-rm -f /opt/JDownloader/JDownloader.pid
+rm -f /opt/JDownloader/app/JDownloader.jar.*
+rm -f /opt/JDownloader/app/JDownloader.pid
 
 # Login user with env credentials - Please prefer command way
 if [ -n "$MYJD_USER" ] && [ -n "$MYJD_PASSWORD" ]; then
@@ -11,7 +11,7 @@ fi
 
 # Defining device name to jdownloader interface - please prefer this method than changing on MyJDownloader to keep correct binding
 if [ -n "$MYJD_DEVICE_NAME" ]; then
-    sed -Ei "s/\"devicename\" : .+\"(,?)/\"devicename\" : \"$MYJD_DEVICE_NAME\"\1/" /opt/JDownloader/cfg/org.jdownloader.api.myjdownloader.MyJDownloaderSettings.json
+    sed -Ei "s/\"devicename\" : .+\"(,?)/\"devicename\" : \"$MYJD_DEVICE_NAME\"\1/" /opt/JDownloader/app/cfg/org.jdownloader.api.myjdownloader.MyJDownloaderSettings.json
 fi
 
 # Debugging helper - if the container crashes, create a file called "jdownloader-block.txt" in the download folder
@@ -20,21 +20,25 @@ if [ -f /opt/JDownloader/Downloads/jdownloader-block.txt ]; then
     sleep 1000000
 fi
 
+# Copy libs if not copied yet
+if [ ! -f /opt/JDownloader/app/libs/sevenzipjbinding1509.jar ]; then
+    cp /opt/JDownloader/libs/*.jar /opt/JDownloader/app/libs/
+fi
+
+# Copy if no JDownloader exists
+if [ ! -f /opt/JDownloader/app/JDownloader.jar ]; then
+    cp /opt/JDownloader/JDownloader.jar /opt/JDownloader/app/
+fi
+
 # Check JDownloader.jar integrity and removes it in case it's not
-jar tvf /opt/JDownloader/JDownloader.jar > /dev/null 2>&1
+jar tvf /opt/JDownloader/app/JDownloader.jar > /dev/null 2>&1
 if [ $? -ne 0 ]; then
-    rm /opt/JDownloader/JDownloader.jar
+    rm /opt/JDownloader/app/JDownloader.jar
 fi
 
 # Check if JDownloader.jar exists, or if there is an interrupted update
-if [ ! -f /opt/JDownloader/JDownloader.jar ] && [ -f /opt/JDownloader/tmp/update/self/JDU/JDownloader.jar ]; then
-    cp /opt/JDownloader/tmp/update/self/JDU/JDownloader.jar /opt/JDownloader/
-fi
-
-# Redownload if no JDownloader exists
-if [ ! -f /opt/JDownloader/JDownloader.jar ]; then
-    wget -O /opt/JDownloader/JDownloader.jar "http://installer.jdownloader.org/JDownloader.jar"
-    chmod +x /opt/JDownloader/JDownloader.jar
+if [ ! -f /opt/JDownloader/app/JDownloader.jar ] && [ -f /opt/JDownloader/app/tmp/update/self/JDU/JDownloader.jar ]; then
+    cp /opt/JDownloader/app/tmp/update/self/JDU/JDownloader.jar /opt/JDownloader/app/
 fi
 
 # Defines umask - should respect octal format
@@ -43,7 +47,7 @@ if echo "$UMASK" | grep -Eq '0[0-7]{3}' ; then
     umask "$UMASK"
 fi
 
-java -Dsun.jnu.encoding=UTF-8 -Dfile.encoding=UTF-8 -Djava.awt.headless=true -jar /opt/JDownloader/JDownloader.jar -norestart &
+java -Dsun.jnu.encoding=UTF-8 -Dfile.encoding=UTF-8 -Djava.awt.headless=true -jar /opt/JDownloader/app/JDownloader.jar -norestart &
 PID=$!
 wait $PID
 wait $PID
